31cc118581e4399db55afa3a61183ea965c3b04c
chenyongzhouking <64853785+chenyongzhouking@users.noreply.github.com>
2021-05-28 15:45:03 +0200

    modified jar,pom,primaryController and SecondaryController,secondary.fxml,primary.fxml.

--- src/main/java/it/polimi/ingsw/view/GUI/GUI.java
+++ src/main/java/it/polimi/ingsw/view/GUI/GUI.java
@@ -1,12 +1,42 @@
 package it.polimi.ingsw.view.GUI;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.util.Observable;
 import java.util.Observer;
 
-public class GUI implements Observer {
+public class GUI extends Application implements Observer {
+
 
     @Override
     public void update(Observable o, Object arg) {
 
+    }
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(loadFXML("primary"), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(GUI.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
