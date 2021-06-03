6c980414482c2791327c83912fe7f6b9589790a3
chenyongzhouking <64853785+chenyongzhouking@users.noreply.github.com>
2021-06-03 16:05:51 +0200

    modified created MarbleMapGUI and modified YourTurnSceneController

---
+++ src/main/java/it/polimi/ingsw/parser/MarbleMapGUI.java
@@ -0,0 +1,23 @@
+package it.polimi.ingsw.parser;
+
+import it.polimi.ingsw.model.market.Marble;
+
+import java.util.HashMap;
+
+public class MarbleMapGUI {
+    private static MarbleMapGUI marbleParser;
+    private static HashMap<String, String> marbleMap = new HashMap<>();
+    private MarbleMapGUI(){
+        marbleMap.put("RED","photos/rossa.png");
+        marbleMap.put("PURPLE","photos/viola.png");
+        marbleMap.put("WHITE","photos/bianca.png");
+        marbleMap.put("BLUE","photos/blu.png");
+        marbleMap.put("GREY","photos/grigio.png");
+        marbleMap.put("YELLOW","photos/gialla.png");
+    }
+    public static String getMarble(Marble marbleID){
+        if(marbleParser == null)
+            marbleParser = new MarbleMapGUI();
+        return marbleMap.get(marbleID.toString());
+    }
+}
--- src/main/java/it/polimi/ingsw/view/GUI/YourTurnSceneController.java
+++ src/main/java/it/polimi/ingsw/view/GUI/YourTurnSceneController.java
@@ -3,14 +3,21 @@ package it.polimi.ingsw.view.GUI;
 import it.polimi.ingsw.model.games.states.GAME_STATES;
 import it.polimi.ingsw.network.client.ClientSocket;
 import it.polimi.ingsw.network.messages.*;
+import it.polimi.ingsw.parser.CardMapGUI;
+import it.polimi.ingsw.view.ClientView;
 import it.polimi.ingsw.view.model_view.Game_View;
+import javafx.application.Platform;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.RadioButton;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 
 import java.io.IOException;
 
@@ -22,8 +29,6 @@ public class YourTurnSceneController extends SceneController{
     private final Game_View game=new Game_View();
     @FXML
     private Button chooseSlot1;
-    @FXML
-    private RadioButton first_action;
     @FXML
     private RadioButton radiobutBuyCard;
     @FXML
@@ -77,6 +82,14 @@ public class YourTurnSceneController extends SceneController{
     @FXML
     private ImageView card43;
     @FXML
+    private ImageView marbleShow1;
+    @FXML
+    private ImageView marbleShow2;
+    @FXML
+    private ImageView marbleShow3;
+    @FXML
+    private ImageView marbleShow4;
+    @FXML
     private Button chooseLeader1;
     @FXML
     private Button chooseLeader2;
@@ -106,9 +119,17 @@ public class YourTurnSceneController extends SceneController{
     private Button no;
     @FXML
     private Button ok;
-
-
-
+    @FXML
+    private Label chat;
+    @FXML
+    private RadioButton radiobutEndProd;
+    @FXML
+    private RadioButton switch1;
+    @FXML
+    private RadioButton switch2;
+    @FXML
+    private RadioButton switch3;
+    private int marbleCount;
     @FXML
     public void initialize(){
         shieldAmount.setText("0");
@@ -132,9 +153,78 @@ public class YourTurnSceneController extends SceneController{
     }
 
 
+    private void chooseSwitch(Message messagee){
+        Message_ArrayList_Marble m = (Message_ArrayList_Marble) messagee;
+        //ClientView.take_marble_message(message);
+        System.out.println("You have chosen this marbles: ");
+        message.setVisible(true);
+        message.setText("Wanna switch your deposit?");
+        yes.setVisible(true);
+        no.setVisible(true);
+        //show marbles
+        yes.addEventHandler(MouseEvent.MOUSE_CLICKED,mouseEvent -> yesSwitch());
+        no.addEventHandler(MouseEvent.MOUSE_CLICKED,mouseEvent -> noSwitch());
+    }
+
+    private void noSwitch(){
+        marbleCount--;
+        if(marbleCount>0)
+            showReducedMarble();
+
+    }
+
+    private void showReducedMarble(){
+
+    }
+
+    private void yesSwitch()
+    {
+        switch1.setDisable(false);
+        switch2.setDisable(false);
+        switch3.setDisable(false);
+        switch1.addEventHandler(MouseEvent.MOUSE_CLICKED,mouseEvent -> {
+            switch1.setDisable(true);
+            clickedSwitch();
+        });
+        switch2.addEventHandler(MouseEvent.MOUSE_CLICKED,mouseEvent -> {
+            switch2.setDisable(true);
+            clickedSwitch();
+        });
+        switch3.addEventHandler(MouseEvent.MOUSE_CLICKED,mouseEvent -> {
+            switch3.setDisable(true);
+            clickedSwitch();
+        });
+    }
+
+    private void clickedSwitch(){
+        int par1=0;
+        int par2=0;
+        if(switch1.isDisable() && switch2.isDisable())
+        {
+            par1=1;
+            par2=2;
+        }
+        if(switch1.isDisable() && switch3.isDisable()){
+            par1=1;
+            par2=3;
+        }
+        if(switch2.isDisable() && switch3.isDisable()){
+            par1=2;
+            par2=3;
+        }
+        if (par1!=0 && par2!=0) {
+            try {
+                ClientSocket.sendMessage(new Message_Two_Parameter_Int(MessageType.SWITCH_DEPOT, GUI.getPosition(), 1, 2));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     private void take_market_marble_row(int i)  {
+        marbleCount=4;
         try {
-            ClientSocket.sendMessage(new Message_Two_Parameter_Int(MessageType.TAKE_MARBLE, position, 0, i));
+            ClientSocket.sendMessage(new Message_Two_Parameter_Int(MessageType.TAKE_MARBLE, GUI.getPosition(), 0, i));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -142,25 +232,13 @@ public class YourTurnSceneController extends SceneController{
 
 
     private void take_market_marble_column(int i)  {
+        marbleCount=3;
         try {
             ClientSocket.sendMessage(new Message_Two_Parameter_Int(MessageType.TAKE_MARBLE, position, 1, i));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
-    private void take_marble_message(Message message) throws IOException {
-        Message_ArrayList_Marble m = (Message_ArrayList_Marble) message;
-        game.setChosenMarbles(m.getMarbles());
-        card12.setVisible(false);
-
-        /*
-        chose_marble(m.getMarbles());
-
-         */
-    }
-
-
 
 
     private void otherPlayerboardButton()  {
@@ -190,6 +268,7 @@ public class YourTurnSceneController extends SceneController{
 
 
     private void TakeMarbleButton(){
+
         disableAllButton();
         row1.setDisable(false);
         row2.setDisable(false);
@@ -198,6 +277,7 @@ public class YourTurnSceneController extends SceneController{
         column2.setDisable(false);
         column3.setDisable(false);
         column4.setDisable(false);
+
     }
 
     private void buyCardButton(){
@@ -218,6 +298,7 @@ public class YourTurnSceneController extends SceneController{
 
 
     private void disableAllButton(){
+        radiobutEndProd.setDisable(true);
         radiobutBuyCard.setDisable(true);
         radiobutActLeader.setDisable(true);
         radiobutActivProduc.setDisable(true);
@@ -235,5 +316,40 @@ public class YourTurnSceneController extends SceneController{
             croceRossa.setLayoutY(croceRossa.getLayoutY()+28);
         else croceRossa.setLayoutX(croceRossa.getLayoutX()+30);
     }
+/*
+    public void update() {
+        Task<Void> task = new Task<Void>() {
+            String a = "Initial text";
+
+            @Override
+            public Void call() throws Exception {
+                int i = 0;
+
+                while (true) {
+
+                    if (i > 4)
+                        a = "I is bigger than 4";
+
+                    if (i > 10)
+                        a = "I is bigger than 10";
+
+                    Platform.runLater(() -> {
+                        testo.setText(a);
+                        // If you want to you can also move the text here
+                        testo.relocate(10, 10);
+                    });
+
+                    i++;
+                    Thread.sleep(1000);
+                }
+            }
+        };
+        Thread th = new Thread(task);
+        th.setDaemon(true);
+        th.start();
+
+    }
+
+ */
 
 }
